dff2155323211abd5f950127fe314f64143da42d
Agbo Frank <agbofrank531@gmail.com>
2025-03-04 09:53:13 +0100

    added the sample file

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -3,7 +3,7 @@ import path from "path";
 export default {
   client: 'sqlite3',
   connection: {
-    filename: path.resolve(__dirname, "../../db.sqlite"),
+    filename: path.resolve(process.cwd(), "db.sqlite"),
   },
   useNullAsDefault: true,
   migration: {
